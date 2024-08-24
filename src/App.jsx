227a6382833994cfa7be1763b56b1fd9376f5da4
atikur-24 <atikurrahman7223@gmail.com>
2024-08-24 11:33:43 +0600

    show banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Achievements from "./components/Achievements/Achievements";
+import Banner from "./components/Banner/Banner";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Projects from "./components/Projects/Projects";
 import Services from "./components/Services/Services";
@@ -8,6 +9,7 @@ import Testimonials from "./components/Testimonials/Testimonials";
 const App = () => {
   return (
     <div>
+      <Banner />
       <Services />
       <Achievements />
       <Projects />
