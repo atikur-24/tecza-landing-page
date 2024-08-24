7d0f3fbc821f5f4ee443c2a90cc2948eba0c78cc
atikur-24 <atikurrahman7223@gmail.com>
2024-08-24 11:29:57 +0600

    hide banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Achievements from "./components/Achievements/Achievements";
-import Banner from "./components/Banner/Banner";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Projects from "./components/Projects/Projects";
 import Services from "./components/Services/Services";
@@ -9,7 +8,6 @@ import Testimonials from "./components/Testimonials/Testimonials";
 const App = () => {
   return (
     <div>
-      {/* <Banner /> */}
       <Services />
       <Achievements />
       <Projects />
