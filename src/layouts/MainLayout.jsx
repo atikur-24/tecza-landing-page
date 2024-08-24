6cce78fa3edab7fa0620c9288b5251f187c111fc
atikur-24 <atikurrahman7223@gmail.com>
2024-08-24 09:07:23 +0600

    create a responsive footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <>
       <h1>Header Section</h1>
       <Outlet />
-      <h1>Footer Section</h1>
+      <Footer />
     </>
   );
 };
